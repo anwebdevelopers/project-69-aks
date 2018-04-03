8a4fb30d6ddfb436fec3be4424d8cfe9c3a04491
anwebdevelopers <anwebdev78@gmail.com>
2018-04-03 19:56:29 +0300

    2.04.2018

--- html/js/common.js
+++ html/js/common.js
@@ -7,7 +7,8 @@ $(function() {
     /*******************************************************/
     $('.dropdown').click(function(e) {
         e.stopPropagation();
-        $(this).toggleClass('active').siblings('.dropdown').removeClass('active');
+        $(this).toggleClass('active');
+        $('.dropdown').not($(this)).removeClass('active');
     });
     $(document).on('click', function(e) {
         if (!$(e.target).closest('.dropdown').length) {
@@ -29,6 +30,48 @@ $(function() {
             $('.main__grid-item').removeClass('active');
         }
         e.stopPropagation();
+    });
+
+
+    /*******************************************************/
+    //SLIDER
+    /*******************************************************/
+    $('.slider').addClass('owl-carousel').owlCarousel({
+        loop: true,
+        items: 1,
+        nav: true,
+        navText: '',
+        autoplayTimeout: 5000,
+        autoplay: true,
+        smartSpeed: 600
+    });
+
+    /*******************************************************/
+    //CAROUSEL
+    /*******************************************************/
+    $('.carousel').addClass('owl-carousel').owlCarousel({
+        loop: true,
+        items: 4,
+        nav: true,
+        navText: '',
+        autoplayTimeout: 5000,
+        autoplay: true,
+        smartSpeed: 600,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            481: {
+                items: 2
+            },
+            769: {
+                items: 3
+            },
+            993: {
+                items: 4
+            }
+        }
     });
 
 });
